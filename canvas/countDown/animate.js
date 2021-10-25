a9b33807a25a4573150c5d9302e6206035bb031a
liuguang <1072941868@qq.com>
2021-10-25 09:43:54 +0800

    canvas: add physics ball animate

---
+++ canvas/countDown/animate.js
@@ -0,0 +1,2 @@
+const balls = [];
+const randomColors = ['#00e09e', '#ffb3a7', '#bddd22', '#75878a', '#70f3ff', '#88ada6', '#ffffff', '#f2be45', '#065279', '#db5a6b', '#fff143', '#622a1d', '#ed5736'];
