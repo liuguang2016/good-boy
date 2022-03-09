0cc1f2e71c22404148eee8ecf31a9e34083390a0
liuguang <liuguang@shiqiao.com>
2022-03-09 09:55:44 +0800

    feat: class genericity

--- ts/src/genericity.ts
+++ ts/src/genericity.ts
@@ -26,3 +26,32 @@ const multiple = <T, P>(first: T, second: P) => {
 };
 
 multiple<string, number>("1", 2);
+
+/**
+ * class中使用泛型
+ */
+
+interface Girl {
+  name: string;
+}
+
+class SelectGirl<T extends Girl> {
+  constructor(private girls: T[]) {}
+  getGirl(index: number): string {
+    return this.girls[index].name;
+  }
+}
+
+const selectGirl = new SelectGirl<Girl>([{ name: "红姐" }, { name: "小南京" }, { name: "小湖南" }]);
+
+const girl = selectGirl.getGirl(0);
+console.log(girl);
+
+/**
+ * 泛型约束
+ */
+const look = <T extends string | number>(param) => {
+  console.log(param);
+};
+
+look<number>(1);
